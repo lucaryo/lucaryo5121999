639d75f8baf4d795473ef6ad41611cc22c1c9652
lucaryo <32738253+lucaryo@users.noreply.github.com>
2022-03-08 12:23:05 +0700

    Update QuestionAnswerComicInteractive.lua

--- ExternalData/Question/SubViews/QuestionAnswerComicInteractive.lua
+++ ExternalData/Question/SubViews/QuestionAnswerComicInteractive.lua
@@ -18,15 +18,15 @@ function SetImage(imgName)
 end
 
 function SetImagePos(x, y, z)
-    _img.SetLocalPosition(x, y, z)
+    --_img.SetLocalPosition(x, y, z)
 end
 
 function SetImageRot(x, y, z, w)
-    _img.SetLocalRotation(x, y, z, w)
+    --_img.SetLocalRotation(x, y, z, w)
 end
 
 function SetImageScale(x, y, z)
-    _img.SetLocalScale(x, y, z)
+    --_img.SetLocalScale(x, y, z)
 end
 
 function Hide()

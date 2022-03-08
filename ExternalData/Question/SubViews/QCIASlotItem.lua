b6c42ed3925b1e2abef29adae10fd83cbbcc13f1
lucaryo <32738253+lucaryo@users.noreply.github.com>
2022-03-08 18:14:21 +0700

    Update QCIASlotItem.lua

--- ExternalData/Question/SubViews/QCIASlotItem.lua
+++ ExternalData/Question/SubViews/QCIASlotItem.lua
@@ -22,11 +22,11 @@ function SetLocalPosition(x, y, z)
 end
 
 function SetLocalRotation(x, y, z, w)
-    _img.SetLocalRotation(x, y, z, w)
+    --_img.SetLocalRotation(x, y, z, w)
 end
 
 function SetLocalScale(x, y, z)
-    _img.SetLocalScale(x, y, z)
+    --_img.SetLocalScale(x, y, z)
 end
 
 function SetRectSize(x, y)
